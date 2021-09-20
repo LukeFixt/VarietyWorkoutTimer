c65950d7560e0fe3ac205b87f72fd5e30da2a951
Luke Fixt <lukeflego@live.com.au>
2021-09-20 18:44:33 +1000

    Basic pagination
    Capitalised page data since using for title

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,24 @@
-const page = $('#page');
+function loadPage(link){
+  const page = $('#page');
+  $(page).load(`pages/${link.toLowerCase()}.html`);
+  $('title').text(link + ' | Variety Workout Timer');
+  window.location = '#' + link;
+}
+
+const links = [];
 $('*[data-page]').each((i,el) => {
   let link = $(el).data('page');
+  links.push(link);
   $(el).on('click',e => {
-    $(page).load(`pages/${link}.html`);
+    loadPage(link);
   });
 });
+
+$(document).ready(e=>{
+  const current = window.location.hash.split('#')[1];
+  if(links.includes(current)){
+    loadPage(current);
+  } else {
+    loadPage('Dashboard');
+  }
+})

aea0e9365827a774ed478e7a7384ca0b49575cdd
Luke Fixt <lukeflego@live.com.au>
2021-09-23 20:17:27 +1000

    Added quick filters on Exercises page

--- src/js/exercises.js
+++ src/js/exercises.js
@@ -1,16 +1,59 @@
 groups = ['Neck', 'Shoulders', 'Upper Arms', 'Lower Arms', 'Chest', 'Abdomen', 'Back', 'Butt', 'Thighs', 'Calves'];
 types = ['Balance', 'Endurance', 'Flexibility', 'Strength'];
 
+chipFilter = $('<span class="chip active"></span>');
+chipFilter.on('click', e => {
+  $(e.target).toggleClass('active');
+  filter($('#exerciseGrid'), e);
+});
+
+groupFilter = $('#group-filter');
+groups.forEach(group => {
+  groupFilter.append(chipFilter.clone(true).text(group));
+});
+
+typeFilter = $('#type-filter');
+types.forEach(type => {
+  typeFilter.append(chipFilter.clone(true).text(type));
+});
+
+function filter(cardParent, item) {
+  let clicked = false;
+  if(item.target != undefined){
+    clicked = true;
+    item = $(item.target);
+  }
+  
+  $(cardParent).children().each((i, card) => {
+    if ($(card).text().includes(item.text())) {
+      if(card.filter == undefined){
+        card.filter = 1;
+      }
+      if (item.hasClass('active') && clicked) {
+        card.filter++;
+      } else {
+        card.filter--;
+      }
+      if(card.filter > 0){
+        $(card).show();
+      } else {
+        $(card).hide();
+      }
+    }
+  });
+
+}
+
 function addExercise(newExercise) {
   const base = {
     title: '',
     desc: '',
     group: random(groups),
     type: random(types),
-    sets: random(3,5),
-    reps: random(10,20),
-    timeActive: random(90,120),
-    timeRest: random(30,60),
+    sets: random(3, 5),
+    reps: random(10, 20),
+    timeActive: random(90, 120),
+    timeRest: random(30, 60),
     media: '',
     total: 0
   }
@@ -18,7 +61,7 @@ function addExercise(newExercise) {
     ...base,
     ...newExercise
   }
-  exercise['total'] = exercise['sets'] * (exercise['timeActive']+exercise['timeRest']);
+  exercise['total'] = exercise['sets'] * (exercise['timeActive'] + exercise['timeRest']);
 
   const temp = $('#tempExercise').clone().removeClass('hidden').removeAttr('id');
   temp.html(
@@ -35,15 +78,30 @@ function addExercise(newExercise) {
       .replace('__media', exercise['media'])
   );
 
-  let active = $('<div class="active"></div>').width((exercise['timeActive']/(exercise['total']/100)).toString()+'%');
-  let rest = $('<div class="rest"></div>').width((exercise['timeRest']/(exercise['total']/100)).toString() +'%');
+  let active = $('<div class="active"></div>').width((exercise['timeActive'] / (exercise['total'] / 100)).toString() + '%');
+  let rest = $('<div class="rest"></div>').width((exercise['timeRest'] / (exercise['total'] / 100)).toString() + '%');
 
-  for(i = 0; i < exercise['sets']; i++){
+  for (i = 0; i < exercise['sets']; i++) {
     active.clone().appendTo(temp.find('.timeline'));
     rest.clone().appendTo(temp.find('.timeline'));
-  }    
+  }
+
+
+  $('.chip:not(.active)',groupFilter).each((i,e)=>{
+    $(e).click().addClass('unclick');
+  })
+  $('.chip:not(.active)',typeFilter).each((i,e)=>{
+    $(e).click().addClass('unclick');
+  })
 
   temp.appendTo($('#exerciseGrid'));
+
+  $('.chip.unclick',groupFilter).each((i,e)=>{
+    $(e).click().removeClass('unclick');
+  })
+  $('.chip.unclick',typeFilter).each((i,e)=>{
+    $(e).click().removeClass('unclick');
+  })
 }
 addExercise({
   title: 'Standing Lunge',
@@ -55,7 +113,7 @@ addExercise({
   timeRest: 60,
 })
 
-$('#add').on('click',e=>{
+$('#add').on('click', e => {
   let name = random(groups);
   addExercise({
     title: name,
